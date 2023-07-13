4cddf266f7b07500a6e34e8d2f98f1e22f872d9c
gznunes <gznunes@gmail.com>
2023-07-12 22:23:56 -0300

    dep

--- dbConfig.js
+++ dbConfig.js
@@ -4,13 +4,13 @@ const { Pool } = require("pg");
 
 const isProduction = process.env.NODE_ENV === "production";
 
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+// const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 // const connectionString = `postgres://webchamada_user:vjGoHcz6H1fVkNftqfrPBcW3cu5EafH7@dpg-cigfuitgkuvojj90m7eg-a.oregon-postgres.render.com/webchamada`;
 // const connectionString = `postgres://webchamada_user:vjGoHcz6H1fVkNftqfrPBcW3cu5EafH7@dpg-cigfuitgkuvojj90m7eg-a/webchamada`;
 
 const pool = new Pool({
 //   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-connectionString: `postgres://webchamada_user:vjGoHcz6H1fVkNftqfrPBcW3cu5EafH7@dpg-cigfuitgkuvojj90m7eg-a/webchamada`,
+connectionString: `postgres://webchamada_user:vjGoHcz6H1fVkNftqfrPBcW3cu5EafH7@dpg-cigfuitgkuvojj90m7eg-a.oregon-postgres.render.com/webchamada`,
 //   ssl: isProduction
   ssl: true
 });
